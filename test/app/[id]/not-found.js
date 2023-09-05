d8c8f5fd0f445dd485c99e8ded5a0b66ec9e7411
youngbfg <bilalfurkangenc@gmail.com>
2023-09-05 11:40:12 +0300

    v0.3

--- test/app/[id]/not-found.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotFound() {
-  return (
-    <div>
-      <h1>Blog doesn't exist</h1>
-    </div>
-  );
-}
--- test/app/test/like.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const like = () => {
-  return (
-    <div>like</div>
-  )
-}
-
-export default like
